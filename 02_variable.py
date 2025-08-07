96b551ee0b02ac65e01123482dc7c5b253ded5be
mdrayan001 <rayanmd641@gmail.com>
2025-08-07 23:00:57 +0530

    variables in python

---
+++ 02_variable.py
@@ -0,0 +1,10 @@
+# In python , variables are used to store data values.
+# Variables are created when you assign a value to them.
+# Variable names can be any length and can include letters, numbers, and underscores.
+# However, they cannot start with a number and should not use reserved keywords.
+# A variable is created the moment you assign a value to it usig the assignment operator (=).
+
+
+name = "md rayan"  # here name is a variable  (integer)
+age = 20  # here age is a variable   (string)
+cgpa = 3.75  # here cgpa is a variable (float)
