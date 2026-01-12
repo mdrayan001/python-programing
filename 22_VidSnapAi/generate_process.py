704c7f53c045c4f1a769c2e4201e5f7cae4c2329
mdrayan001 <rayanmd641@gmail.com>
2026-01-12 23:34:30 +0530

    modified the create reel function

--- 22_VidSnapAi/generate_process.py
+++ 22_VidSnapAi/generate_process.py
@@ -2,6 +2,7 @@
 import os 
 from text_to_audio import text_to_speech_file
 import time
+import subprocess
 
 
 def text_to_audio(folder):
@@ -9,9 +10,12 @@ def text_to_audio(folder):
     with open(f"user_uploads/{folder}/desc.txt") as f:
         text = f.read()
     print(text, folder)
-    # text_to_speech_file(text, folder)
+    text_to_speech_file(text, folder)
 
 def create_reel(folder):
+    command = f'''ffmpeg -f concat -safe 0 -i user_uploads/{folder}/input.txt -i user_uploads/{folder}/audio.mp3 -vf "scale=1080:1920:force_original_aspect_ratio=decrease,pad=1080:1920:(ow-iw)/2:(oh-ih)/2:black" -c:v libx264 -c:a aac -shortest -r 30 -pix_fmt yuv420p static/reels/{folder}.mp4'''
+    subprocess.run(command, shell=True, check=True)
+    
     print("CR - ", folder)
 
 if __name__ == "__main__":
